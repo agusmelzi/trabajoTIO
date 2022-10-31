8c779c5902214f5de528ea19c548f5d1e11a29a9
viglezziR <viglezziramiro@gmail.com>
2022-10-31 12:45:11 -0300

    se genero archivo completarTabla.js y su funcion cargarTabla

---
+++ js/completarTabla.js
@@ -0,0 +1,32 @@
+"use strict"
+
+let url = 'https://62c38328876c4700f53d60be.mockapi.io/api/libros';
+let formulario = document.querySelector("#formAgregarLibro");
+formulario.addEventListener("submit", nuevoIngreso);
+
+window.addEventListener("load", cargarTabla);
+
+async function cargarTabla() {
+
+    let contenidoTabla = document.querySelector("#contenidoTabla");
+    contenidoTabla.innerHTML = "";
+
+    try {
+        let res = await fetch(url);
+        let tabla = await res.json();
+        console.log(tabla);
+        for (const libro of tabla) {
+            //let id = libro.id;
+            contenidoTabla.innerHTML += `<tr><td>${libro.titulo}</td><td>${libro.autor}</td><td>${libro.editorial}
+            </td><td>${libro.anio}</td>
+            <td><button class="borrarItem" id="borrarItem"><i class="fa-solid fa-trash borrar"></i></button></td>
+            <td><button class="editarItem" data-id="${libro.id}"><i class="fa-solid fa-pen-to-square editar"></i></button></td></tr>`;
+            console.log(contenidoTabla);
+        }
+        asignarItemDelete();
+        asignarItemEdit();
+    } catch (error) {
+        console.log(error);
+    }
+
+}
